a824b68785e5e791c013705f538b509bff66cee0
Szymon Gryszko <gryszkoszymon@gmail.com>
2020-06-03 17:02:28 +0200

    loading widgets in ProfileActivity from database

--- app/src/main/java/com/example/instaclone/utils/FirebaseMethods.java
+++ app/src/main/java/com/example/instaclone/utils/FirebaseMethods.java
@@ -13,6 +13,7 @@ import com.example.instaclone.R;
 import com.example.instaclone.home.HomeActivity;
 import com.example.instaclone.model.User;
 import com.example.instaclone.model.UserAccountSettings;
+import com.example.instaclone.model.UserAndSettings;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -44,12 +45,56 @@ public class FirebaseMethods {
         }
     }
 
+
+    public UserAndSettings getUserAndAccountSettings(DataSnapshot dataSnapshot) {
+        Log.d(TAG, "getUserAndAccountSettings: getting user info from firebase");
+
+        User user = new User();
+        UserAccountSettings settings = new UserAccountSettings();
+
+        for (DataSnapshot ds : dataSnapshot.getChildren()) {
+
+            if (ds.getKey().equals(context.getString(R.string.dbname_user_account_settings))) {
+                try {
+
+                    settings.setDescription(ds.child(userID).getValue(UserAccountSettings.class).getDescription());
+                    settings.setDisplay_name(ds.child(userID).getValue(UserAccountSettings.class).getDisplay_name());
+                    settings.setFollowers(ds.child(userID).getValue(UserAccountSettings.class).getFollowers());
+                    settings.setFollowing(ds.child(userID).getValue(UserAccountSettings.class).getFollowing());
+                    settings.setPosts(ds.child(userID).getValue(UserAccountSettings.class).getPosts());
+                    settings.setProfile_photo(ds.child(userID).getValue(UserAccountSettings.class).getProfile_photo());
+                    settings.setUsername(ds.child(userID).getValue(UserAccountSettings.class).getUsername());
+                    settings.setWebsite(ds.child(userID).getValue(UserAccountSettings.class).getWebsite());
+
+                } catch (NullPointerException e) {
+                    Log.e(TAG, "getUserAndAccountSettings: Null pointer Exception" + e.getMessage() );
+                }
+            }
+
+            if (ds.getKey().equals(context.getString(R.string.dbname_users))) {
+                try {
+                    user.setUser_id(ds.child(userID).getValue(User.class).getUser_id());
+                    user.setEmail(ds.child(userID).getValue(User.class).getEmail());
+                    user.setPhone_number(ds.child(userID).getValue(User.class).getPhone_number());
+                    user.setUsername(ds.child(userID).getValue(User.class).getUsername());
+
+
+                } catch (NullPointerException e) {
+                    Log.e(TAG, "getUserAndAccountSettings: Null pointer Exception" + e.getMessage() );
+                }
+            }
+        }
+
+        return new UserAndSettings(user, settings);
+
+    }
+
     public void addNewUser(String email, String username, String description, String website, String profile_photo) {
 
         User user = new User(userID, email, 0, StringManipulation.condenseUsername(username));
         myRef.child(context.getString(R.string.dbname_users)).child(userID).setValue(user);
 
-        UserAccountSettings settings = new UserAccountSettings(description, username, 0, 0, 0, profile_photo, username, website);
+        UserAccountSettings settings = new UserAccountSettings(description, username, 0, 0, 0, profile_photo, StringManipulation.condenseUsername(username), website);
         myRef.child(context.getString(R.string.dbname_user_account_settings)).child(userID).setValue(settings);
 
     }
